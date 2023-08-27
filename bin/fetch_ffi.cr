9fecf04310334fc5f4b9f8d290dcad7b89e66932
Yousaf Nabi <you@saf.dev>
2023-08-27 06:26:29 +0100

    feat: 14mb ffi with libpact_ffi.a

--- bin/fetch_ffi.cr
+++ bin/fetch_ffi.cr
@@ -4,9 +4,12 @@ require "file_utils"
 # require "tempfile"
 
 LIB_DIR     = File.expand_path(File.dirname(__FILE__))
-FFI_VERSION = "v0.4.7"
+FFI_VERSION = "v0.4.5"
+# FFI_VERSION = "v0.4.7"
 # FFI_VERSION = ENV["FFI_VERSION"] || "0.3.4"
-BASEURL = "https://github.com/pact-foundation/pact-reference/releases/download"
+# Using fork to get .a binarys for all platforms
+BASEURL = "https://github.com/you54f/pact-reference/releases/download"
+# BASEURL = "https://github.com/pact-foundation/pact-reference/releases/download"
 FFI_DIR = File.expand_path("#{LIB_DIR}/..")
 {% if flag?(:darwin) %}
 ENV["SSL_CERT_FILE"] ||= "/etc/ssl/cert.pem"
@@ -69,7 +72,8 @@ def main
   # https://crystal-lang.org/reference/1.9/syntax_and_semantics/compile_time_flags.html#querying-flags
   {% if flag?(:linux) && flag?(:x86_64) %}
     system = "linux-x86_64"
-    download_ffi("linux-x86_64.so.gz", "lib", "libpact_ffi.so.gz")
+    # download_ffi("linux-x86_64.so.gz", "lib", "libpact_ffi.so.gz")
+    download_ffi("x86_64-unknown-linux-musl.a.gz", "lib", "libpact_ffi.a.gz")
   {% elsif flag?(:darwin) && flag?(:x86_64) %}
     download_ffi("osx-x86_64.dylib.gz", "lib", "libpact_ffi.dylib.gz")
     system = "osx-x86_64"
@@ -78,7 +82,10 @@ def main
     download_ffi("windows-x86_64.dll.gz", "", "pact_ffi.dll.gz")
   {% elsif flag?(:linux) && flag?(:aarch64) %}
     system = "linux-aarch64"
-    download_ffi("linux-aarch64.so.gz", "lib", "libpact_ffi.so.gz")
+    download_ffi("aarch64-unknown-linux-musl.a.gz", "lib", "libpact_ffi.a.gz")
+    # download_ffi("linux-aarch64.a.gz", "lib", "libpact_ffi.a.gz")
+    # download_ffi("aarch64-unknown-linux-musl.so.gz", "lib", "libpact_ffi.so.gz")
+    # download_ffi("linux-aarch64.so.gz", "lib", "libpact_ffi.so.gz")
   {% elsif flag?(:darwin) && flag?(:aarch64) %}
     system = "osx-aarch64"
     download_ffi("osx-aarch64-apple-darwin.dylib.gz", "lib", "libpact_ffi.dylib.gz")
